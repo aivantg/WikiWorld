b6bc1ad7245865e9e2f179f3750ada559cb1bf8b
aivantg <aivant.developer@gmail.com>
2018-05-17 14:43:43 -0500

    moved sql scripts to separate folder

--- scripts/process.sh
+++ scripts/process.sh
@@ -5,15 +5,15 @@ DBName=$2
 function process() {
   echo $1
   echo $2
-  sed -e "s/tbname/$1/g" process_template.sql > process_tb.sql
-  sed -i '' -e "s/month-string/$2/g" process_tb.sql
-  eval "mysql $DBParams $DBName < process_tb.sql"
-  rm process_tb.sql
+  sed -e "s/tbname/$1/g" sql/process_template.sql > sql/process_tb.sql
+  sed -i '' -e "s/month-string/$2/g" sql/process_tb.sql
+  eval "mysql $DBParams $DBName < sql/process_tb.sql"
+  rm sql/process_tb.sql
 }
 
-eval "mysql $DBParams $DBName < process_reset.sql"
+eval "mysql $DBParams $DBName < sql/process_reset.sql"
 while IFS= read -r -a array
 do
   process ${array[0]} ${array[1]}
 done
-eval "mysql $DBParams $DBName < process_index.sql"
+eval "mysql $DBParams $DBName < sql/process_index.sql"
