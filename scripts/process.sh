7497f3e4c6255b2afe5cbbdd0f9cdd07c7cc245b
aivantg <aivant.developer@gmail.com>
2018-10-18 00:19:11 -0700

    updated process_tb

--- scripts/process.sh
+++ scripts/process.sh
@@ -5,9 +5,9 @@ DBName=$2
 function process() {
   echo $1
   echo $2
-  sed -e "s/tbname/$1/g" sql/process_template.sql > sql/process_tb.sql
-  sed -i -e "s/month-string/$2/g" sql/process_tb.sql
-  # eval "mysql $DBParams $DBName < sql/process_tb.sql"
+  # sed -e "s/tbname/$1/g" sql/process_template.sql > sql/process_tb.sql
+  # sed -e "s/month-string/$2/g" sql/process_tb.sql > sql/process_tb.sql
+  eval "mysql $DBParams $DBName < sql/process_tb.sql"
   # rm sql/process_tb.sql
 }
 
--- scripts/sql/process_tb.sql
+++ scripts/sql/process_tb.sql
@@ -1,16 +1,129 @@
 INSERT INTO page (title)
   SELECT dest
-    FROM  UNION
+    FROM mini201801 UNION
   SELECT src
-    FROM 
+    FROM mini201801
     ON DUPLICATE KEY
       UPDATE title=title;
 
-SELECT STR_TO_DATE('', '%Y-%m-%d') INTO @date;
+SELECT STR_TO_DATE('2018-01-01', '%Y-%m-%d') INTO @date;
 
 INSERT INTO request (srcID, destID, month, type, count)
   SELECT src.id, dest.id, @date, req.type, req.count
-    FROM  as req, page as src, page as dest
+    FROM mini201801 as req, page as src, page as dest
+    WHERE src.title=req.src and dest.title=req.dest
+    ON DUPLICATE KEY
+      UPDATE
+        request.count=request.count+req.count;
+
+
+INSERT INTO page (title)
+  SELECT dest
+    FROM mini201806 UNION
+  SELECT src
+    FROM mini201806
+    ON DUPLICATE KEY
+      UPDATE title=title;
+
+SELECT STR_TO_DATE('2018-06-01', '%Y-%m-%d') INTO @date;
+
+INSERT INTO request (srcID, destID, month, type, count)
+  SELECT src.id, dest.id, @date, req.type, req.count
+    FROM mini201806 as req, page as src, page as dest
+    WHERE src.title=req.src and dest.title=req.dest
+    ON DUPLICATE KEY
+      UPDATE
+        request.count=request.count+req.count;
+
+
+INSERT INTO page (title)
+  SELECT dest
+    FROM mini201807 UNION
+  SELECT src
+    FROM mini201807
+    ON DUPLICATE KEY
+      UPDATE title=title;
+
+SELECT STR_TO_DATE('2018-07-01', '%Y-%m-%d') INTO @date;
+
+INSERT INTO request (srcID, destID, month, type, count)
+  SELECT src.id, dest.id, @date, req.type, req.count
+    FROM mini201807 as req, page as src, page as dest
+    WHERE src.title=req.src and dest.title=req.dest
+    ON DUPLICATE KEY
+      UPDATE
+        request.count=request.count+req.count;
+
+
+INSERT INTO page (title)
+  SELECT dest
+    FROM mini201808 UNION
+  SELECT src
+    FROM mini201808
+    ON DUPLICATE KEY
+      UPDATE title=title;
+
+SELECT STR_TO_DATE('2018-08-01', '%Y-%m-%d') INTO @date;
+
+INSERT INTO request (srcID, destID, month, type, count)
+  SELECT src.id, dest.id, @date, req.type, req.count
+    FROM mini201808 as req, page as src, page as dest
+    WHERE src.title=req.src and dest.title=req.dest
+    ON DUPLICATE KEY
+      UPDATE
+        request.count=request.count+req.count;
+
+INSERT INTO page (title)
+  SELECT dest
+    FROM mini201809 UNION
+  SELECT src
+    FROM mini201809
+    ON DUPLICATE KEY
+      UPDATE title=title;
+
+SELECT STR_TO_DATE('2018-09-01', '%Y-%m-%d') INTO @date;
+
+INSERT INTO request (srcID, destID, month, type, count)
+  SELECT src.id, dest.id, @date, req.type, req.count
+    FROM mini201809 as req, page as src, page as dest
+    WHERE src.title=req.src and dest.title=req.dest
+    ON DUPLICATE KEY
+      UPDATE
+        request.count=request.count+req.count;
+
+
+INSERT INTO page (title)
+  SELECT dest
+    FROM mini201711 UNION
+  SELECT src
+    FROM mini201711
+    ON DUPLICATE KEY
+      UPDATE title=title;
+
+SELECT STR_TO_DATE('2017-11-01', '%Y-%m-%d') INTO @date;
+
+INSERT INTO request (srcID, destID, month, type, count)
+  SELECT src.id, dest.id, @date, req.type, req.count
+    FROM mini201711 as req, page as src, page as dest
+    WHERE src.title=req.src and dest.title=req.dest
+    ON DUPLICATE KEY
+      UPDATE
+        request.count=request.count+req.count;
+
+
+INSERT INTO page (title)
+  SELECT dest
+    FROM mini201712 UNION
+  SELECT src
+    FROM mini201712
+    ON DUPLICATE KEY
+      UPDATE title=title;
+
+SELECT STR_TO_DATE('2017-12-01', '%Y-%m-%d') INTO @date;
+
+INSERT INTO request (srcID, destID, month, type, count)
+  SELECT src.id, dest.id, @date, req.type, req.count
+    FROM mini201712 as req, page as src, page as dest
     WHERE src.title=req.src and dest.title=req.dest
     ON DUPLICATE KEY
       UPDATE
